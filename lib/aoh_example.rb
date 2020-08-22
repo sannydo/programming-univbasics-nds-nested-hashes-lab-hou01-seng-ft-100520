a3c23c4c5544242fb5dfd1cfe5ab9570052a33d9
Sanny Do <sanny.do94@gmail.com>
2020-08-22 06:41:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,11 +8,13 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh 
-  result = [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
-       {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
-       {:name=>"Pat Monteseian", :occupation=>"Staff"},
-       {:name=>"Kate Gompert", :occupation=>"None"},
-       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
+  result = [
+    {:name=>"Don Gately", :occupation=>"Live-in Staff"},
+    {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+    {:name=>"Pat Monteseian", :occupation=>"Staff"},
+    {:name=>"Kate Gompert", :occupation=>"None"},
+    {:name=>"Bruce Green", :occupation=>"Fan of Mildred"},
+    ]
 end
 
 def literal_aoh
